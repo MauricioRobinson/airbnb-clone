d8bc8d74fb89356121b2ecff0a7028d64a800aeb
Mauricio Robinson <marsjr97@gmail.com>
2022-12-16 12:15:48 -0500

    Date range calendar done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,48 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import {
   MagnifyingGlassCircleIcon,
   GlobeAltIcon,
   UsersIcon,
   Bars3Icon,
 } from "@heroicons/react/24/solid";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRangePicker } from "react-date-range";
 
 const Header = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
+
+  const handleSearch = (e) => {
+    const { name, value } = e.target;
+
+    setSearchInput(value);
+  };
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const handleGuests = (e) => {
+    const { value } = e.target;
+
+    setNumberOfGuests(value);
+  };
+
+  const resetInput = () => {
+    setSearchInput("");
+  };
+
   return (
     <header className="w-full shadow-md sticky top-0 px-4 grid grid-cols-3 z-50 md:px-4 md:py-2 bg-white">
       <div className="relative w-24 h-20 cursor-pointer my-auto">
@@ -21,7 +56,9 @@ const Header = () => {
       <div className="my-auto">
         <div className="md:border-2 md:border-red-400 rounded-full py-2 flex items-center md:shadow-sm">
           <input
-            type="search"
+            type="text"
+            value={searchInput}
+            onChange={handleSearch}
             placeholder="Start your search"
             className="outline-none pl-5 bg-transparent flex-grow text-gray-600 placeholder-gray-400"
           />
@@ -37,6 +74,40 @@ const Header = () => {
           <UsersIcon className="w-6 h-6" />
         </div>
       </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center justify-between mb-6 border-b">
+            <h2 className="text-xl pl-2 font-semibold flex-grow">
+              Number of Guests
+            </h2>
+            <UsersIcon className="w-6 h-6 text-black" />
+            <input
+              type="number"
+              onChange={handleGuests}
+              value={numberOfGuests}
+              min={1}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+          <div className="mb-8">
+            <div className="flex">
+              <button
+                onClick={resetInput}
+                className="flex-grow text-gray-500">
+                Cancel
+              </button>
+              <button className="flex-grow text-red-400">Search</button>
+            </div>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
